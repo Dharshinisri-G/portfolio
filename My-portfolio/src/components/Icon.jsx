927ee144478320d4b445e0456e03f6f71e23a5b4
Dharshinisri-G <dharshinisrigajendran@gmail.com>
2024-10-13 22:44:57 +0530

    Installed tailwindcss

---
+++ My-portfolio/src/components/Icon.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Icon = () => {
+  return (
+    <>
+    <div className='bg-red-500 w-15 h-15 text-pink-800'>Icon</div>
+    </>
+  )
+}
+
+export default Icon
---
+++ My-portfolio/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Icon from '../components/Icon'
+
+const Home = () => {
+  return (
+    <>
+    <Icon/>
+    </>
+  )
+}
+
+export default Home
